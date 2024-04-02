664e3dcd09ff98045c3b98fab112f77298fa76b2
corkr933 <corkr933@student.liu.se>
2024-04-02 17:12:50 +0200

    Added more props to the Card component so personalized styling between different elements is easier. Also added first draft of Sign up part of the Sign in page

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -5,12 +5,19 @@ import "../styles/Card.css"
 interface CardProps {
     header: React.ReactNode;
     content: React.ReactNode;
+    height?: number | string;
+    width?: number | string;
+    maxWidth?: number | string;
 };
 
 const Card: FC<CardProps> = props => {
 
     return (
-        <div className="card-container">
+        <div className="card-container" style={{
+            height: props.height,
+            width: props.width,
+            maxWidth: props.maxWidth,
+        }}>
             <div className="card-header">{props.header}</div>
             {props.content}
         </div>
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -3,8 +3,8 @@ import Card from "../components/Card"
 
 export function SigninPage() {
 
-    const header = <h2>Sign in</h2>;
-    const content = (
+    const headerSignin = <h2>Sign in</h2>;
+    const contentSignin = (
         <form className="signin-form">
             <label className="signin-label">
                 Username:
@@ -18,10 +18,26 @@ export function SigninPage() {
         </form>
     );
 
+    const headerSignup = <h2>Not a member yet?</h2>
+    const contentSignup = (
+        <div className="signup-container">
+            <p>With a WhateverThisWebsiteWillBeCalled account you can:<br />
+               - Showcase your football knowledge by participating in the
+               predictions for the Euro 2024 tournament. <br />
+               - Create leagues and compare your predictions to your friends and family. <br />
+               - Perhaps do some quizzes or whatever I add if this stuff isn't enough stuff.
+            </p>
+            <button className="signup-redirect-button">
+                Sign up now!
+            </button>
+        </div>
+    );
+
     return (
 
         <div className="signin-container">
-            <Card header={header} content={content} />
+            <Card header={headerSignin} content={contentSignin} height="300px" width="50%" maxWidth="600px" />
+            <Card header={headerSignup} content={contentSignup} width="50%" maxWidth="600px" />
         </div>
     );
 }
